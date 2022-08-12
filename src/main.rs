5c87dd4058b95554359f8b117c4f4b210984a112
dadhi <max.volkov@gmail.com>
2022-08-12 12:30:47 +0200

    rul110: replace the cells array with u32

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn rule_110() -> Result<()> {
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
 
-    const GEN_SIZE: usize = 30;
+    // const GEN_SIZE: usize = 30;
     const SYMBOLS: [&[u8; 1]; 2] = [b"-", b"+"];
     const D: u8 = 48;
     const DIGITS: [u8; 10] = [D+0, D+1, D+2, D+3, D+4, D+5, D+6, D+7, D+8, D+9];
@@ -23,25 +23,30 @@ fn rule_110() -> Result<()> {
     
     stdout.write(b"## RULE-110 premier. 110 in binary representation is 01101110:\n");
 
-    let mut cells = [0u8; GEN_SIZE];
-    cells[cells.len() - 1] = 1; // init the last cell with 1
-    cells[0] = 1; // init the last cell with 1
+    let mut cells: u32 = 0;
+    cells |= 1 << 31; // init the last cell with 1
 
-    for d1 in 0..4 {
+
+    for d1 in 0..3 {
         for d0 in 0..10 {
             
             stdout.write(&[DIGITS[d1], DIGITS[d0], SPACE])?;
 
-            stdout.write(SYMBOLS[cells[0] as usize])?;
-            stdout.write(SYMBOLS[cells[1] as usize])?;
+            stdout.write(SYMBOLS[(cells & 1) as usize])?; // @perf get rid of usize
+            stdout.write(SYMBOLS[((cells >> 1) & 1) as usize])?;
 
-            let mut pattern = cells[0] << 1 | cells[1];
+            let mut pattern = ((cells & 1) << 1) | ((cells >> 1) & 1);
 
-            for i in 2..GEN_SIZE {
-                stdout.write(SYMBOLS[cells[i] as usize])?;
+            for i in 2..32 {
+                stdout.write(SYMBOLS[((cells >> i) & 1) as usize])?;
 
-                pattern = (pattern << 1 | cells[i]) & 0b0000_0111; // keep pattern as 3 lower bits, 000, 001, 010, 011, etc.
-                cells[i - 1] = (110 >> pattern) & 1; // convert to index in binary representation of 110, and find set bit
+                pattern = (pattern << 1 | ((cells >> i) & 1)) & 0b0000_0111; // keep pattern as 3 lower bits, 000, 001, 010, 011, etc.
+
+                // clear i - 1 bit first
+                cells &= !(1 << (i - 1));
+                // convert to index in binary representation of 110, and set i - 1 bit
+                cells |= ((110 >> pattern) & 1) << (i - 1);
+
             }
             // stdout new line
             stdout.write(b"\n")?;
