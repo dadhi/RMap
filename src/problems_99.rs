1c1515c20eb2dcd0f228022ad8700c63256e9cd1
dadhi <max.volkov@gmail.com>
2022-08-20 23:41:31 +0200

    first 2 problems passing

--- src/problems_99.rs
+++ src/problems_99.rs
@@ -1,27 +1,36 @@
 #[cfg(test)]
 pub mod tests {
 
-    trait Problems<A> {
-        /// Problem 1: Write a function last : 'a list -> 'a option that returns the last element of a list
-        fn p_last(&self) -> Option<&A>;
-    }
-
-    impl<A> Problems<A> for &[A] {
-        fn p_last(&self) -> Option<&A> {
-            match self {
-                [] => None,
-                [last] => Some(last),
-                [_, tail @ ..] => self.last(),
-            }
+    /// Problem 1: Write a function last : 'a list -> 'a option that returns the last element of a list
+    fn p_last<T>(xs: &[T]) -> Option<&T> {
+        match xs {
+            [] => None,
+            [last] => Some(last),
+            [_, rest @ ..] => p_last(rest),
         }
     }
 
+    /// Problem 2: Last two elements of a list
+    fn p_last_two<A>(xs: &[A]) -> Option<(&A, &A)> {
+        match xs {
+            [] | [_] => None,
+            [pre_last, last] => Some((pre_last, last)),
+            [_, rest @ ..] => p_last_two(rest),
+        }
+    }
 
     #[test]
-    fn test_last() {
-        assert_eq!([1, 2, 3, 4].as_ref().p_last(), Some(&4));
-        assert_eq!([1].as_ref().p_last(), Some(&1));
-        let empty_slice: &[i32] = &[];
-        assert_eq!(empty_slice.p_last(), None);
-    } 
+    fn test_p_last() {
+        assert_eq!(p_last(&[1, 2, 3, 4]), Some(&4));
+        assert_eq!(p_last(&[1]), Some(&1));
+        assert_eq!(p_last(&[] as &[i32]), None);
+    }
+
+    #[test]
+    fn test_з_last_two() {
+        assert_eq!(p_last_two(&[1, 2, 3, 4]), Some((&3, &4)));
+        assert_eq!(p_last_two(&[1, 4]), Some((&1, &4)));
+        assert_eq!(p_last_two(&[1]), None);
+        assert_eq!(p_last_two(&[] as &[i32]), None);
+    }
 }
