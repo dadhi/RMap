7eee937f28f714a163787d17784030dbe0407e45
dadhi <max.volkov@gmail.com>
2022-08-22 11:29:12 +0200

    add head

--- src/im_list.rs
+++ src/im_list.rs
@@ -23,6 +23,10 @@ impl<T> ImList<T> {
         }))}
     }
 
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
     pub fn tail(&self) -> Self {
         Self { head: self.head.as_ref().and_then(|node| node.next.clone()) }
     }
@@ -41,5 +45,17 @@ mod tests {
         assert_eq!(list.head.as_ref().unwrap().elem, 3);
         assert_eq!(list.tail().head.as_ref().unwrap().elem, 2);
         assert_eq!(list.tail().tail().head.as_ref().unwrap().elem, 1);
+    }
+
+    #[test]
+    fn test_head_and_tail() {
+        let list = ImList::new();
+        let list = list.prepend(1);
+        let list = list.prepend(2);
+        let list = list.prepend(3);
+        assert_eq!(list.head(), Some(&3));
+        assert_eq!(list.tail().head(), Some(&2));
+        assert_eq!(list.tail().tail().head(), Some(&1));
+        assert_eq!(list.tail().tail().tail().head(), None);
     }
 }
