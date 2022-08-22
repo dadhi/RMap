01631c5b61a13ec17516769745e940a402c14b3e
dadhi <max.volkov@gmail.com>
2022-08-22 11:22:58 +0200

    added prepend

--- src/im_list.rs
+++ src/im_list.rs
@@ -11,6 +11,19 @@ struct Node<T> {
     next: Link<T>,
 }
 
+impl<T> ImList<T> {
+    pub fn new() -> Self {
+        Self { head: None }
+    }
+
+    pub fn prepend(&self, elem: T) -> Self {
+        Self { head: Some(Rc::new(Node {
+            elem: elem,
+            next: self.head.clone(),
+        }))}
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
